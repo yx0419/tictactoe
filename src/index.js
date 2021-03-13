12306062187789c43c549a89830dfca403d258cd
Yunjae Cho <yunxics@gmail.com>
2021-03-12 23:08:09 -0800

    determine who is winner and if there is no winner

--- src/index.js
+++ src/index.js
@@ -32,12 +32,27 @@ class Board extends React.Component {
     ); //pass prop from Board (=parent component) to Square (=child component).
   }
   render() {
-    const who = this.state.isCurrentTurnX ? "X" : "O";
-    const status = "Next player: " + who;
+    let s = this.state.isCurrentTurnX ? "X" : "O";
+    let result = "Next player is " + s;
+
+    let r = calculateWinner(this.state.arrOfSquareValues);
+
+    //let count = 0; //I tried this to check if game is over without winner (all filled), but this won't work because every time user clicks square btn, count will be set to zero again, never can become 9.
+    if (r) {
+      //if r is not null
+      result = "Winner is " + r;
+    } else {
+      //if r is null
+      let nArr = this.state.arrOfSquareValues.filter((x) => x === null);
+      if (nArr.length === 0) {
+        //if 9 values are all non-null, (to know this, use filter method and check if the new array's length is zero)
+        result = "No Winner, all filled";
+      }
+    }
 
     return (
       <div>
-        <div className="status">{status}</div>
+        <div className="status">{result}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -77,3 +92,30 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
+
+function calculateWinner(arrOfSquareValues) {
+  const arrOfWinning = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < arrOfWinning.length; i++) {
+    const [a, b, c] = arrOfWinning[i];
+
+    if (
+      //if these three values are all same,
+      arrOfSquareValues[a] === arrOfSquareValues[b] &&
+      arrOfSquareValues[b] === arrOfSquareValues[c]
+    ) {
+      let winner = arrOfSquareValues[a]; //then it means there is winner.
+      return winner;
+    }
+  }
+  return null;
+}
